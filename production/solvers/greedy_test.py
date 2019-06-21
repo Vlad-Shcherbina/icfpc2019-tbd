53aa70b837fb15eb550c5a9601f669b45fdbf9bd
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2019-06-21 19:45:53 +0300

    Check greedy solver output with validator

--- production/solvers/greedy_test.py
+++ production/solvers/greedy_test.py
@@ -3,11 +3,14 @@ from pathlib import Path
 from production import utils
 from production.data_formats import *
 from production.solvers.greedy import solve
-
+from production.golden import validate
 
 def test_greedy_on_example():
     s = Path(utils.project_root() / 'tasks' / 'part-1-examples' / 'example-01.desc').read_text()
     task = Task.parse(s)
     sol = solve(task)
-    print(sol)
-    # TODO: validate
+
+    res = validate.run(s, compose_actions(sol))
+    print(res)
+    assert res.time is not None
+
