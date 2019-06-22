0adde834f47095dc8c725d9d2768e6ab80f2c5b9
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2019-06-22 05:09:41 +0300

    Uploading of teleporters and clones tasks

--- production/upload_tasks.py
+++ production/upload_tasks.py
@@ -2,6 +2,8 @@ import logging
 logger = logging.getLogger(__name__)
 import json
 import zlib
+from zipfile import ZipFile
+import re
 
 from production import db
 from production import utils
@@ -16,31 +18,40 @@ def main():
     cur = conn.cursor()
 
     legends = {}
-    with (utils.project_root() / 'tasks' / 'part-1-initial' / 'part-1-legend.txt').open() as fin:
-        for line in fin:
-            name, legend = line.split(' - ', maxsplit=1)
-            legends[name] = legend.rstrip()
+    for part_number, part_name in (1, 'initial'), (2, 'teleports'), (3, 'clones'):
+        with ZipFile(utils.project_root() / 'tasks' / f'part-{part_number}-{part_name}.zip') as z:
+            with z.open(f'part-{part_number}-legend.txt') as fin:
+                for line in fin:
+                    name, legend = line.decode().split(' - ', maxsplit=1)
+                    legends[name] = legend.rstrip()
 
-    for n in range(1, 150 + 1):
-        name = f'prob-{n:03d}'
-        task = utils.get_problem_raw(n)
-        data = zlib.compress(task.encode())
-        extra = dict(legend=legends[name])
+            for filename in z.namelist():
+                if filename.endswith('-legend.txt'):
+                    continue
+                m = re.match(r'(prob-\d{3}).desc$', filename)
+                assert m, filename
+                name = m.group(1)
 
-        cur.execute('''
-            INSERT INTO tasks(
-                name, data, extra, invocation_id, time)
-            VALUES (%s, %s, %s, %s, NOW())
-            ON CONFLICT DO NOTHING
-            RETURNING id
-            ''',
-            [name, data, json.dumps(extra), db.get_this_invocation_id(conn)])
-        res = cur.fetchall()
-        if res:
-            [[task_id]] = res
-            logger.info(f'Uploaded {name!r} as /task/{task_id}')
-        else:
-            logger.info(f'Task {name!r} already exists')
+                with z.open(filename, 'r') as fin:
+                    task = fin.read().decode()
+
+                data = zlib.compress(task.encode())
+                extra = dict(legend=legends[name])
+
+                cur.execute('''
+                    INSERT INTO tasks(
+                        name, data, extra, invocation_id, time)
+                    VALUES (%s, %s, %s, %s, NOW())
+                    ON CONFLICT DO NOTHING
+                    RETURNING id
+                    ''',
+                    [name, data, json.dumps(extra), db.get_this_invocation_id(conn)])
+                res = cur.fetchall()
+                if res:
+                    [[task_id]] = res
+                    logger.info(f'Uploaded {name!r} as /task/{task_id}')
+                else:
+                    logger.info(f'Task {name!r} already exists')
 
     db.record_this_invocation(conn, status=db.Stopped())
     conn.commit()
