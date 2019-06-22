addf0fefb0f8638782e53ac81910513bc3857108
Thoughtflare <mrisaaccweiner@gmail.com>
2019-06-23 01:48:45 +0300

    Implement stalactites, but it is not enough

--- scratch/manpages/mites.py
+++ scratch/manpages/mites.py
@@ -1,7 +1,7 @@
 import sys
 
 from production.data_formats import Puzzle, Task
-from production.geom import Pt
+from production.geom import Pt, rasterize_poly
 from typing import List, Optional
 
 '''
@@ -14,22 +14,13 @@ def solve(puz: Puzzle) -> Task:
                 Pt(puz.size, 0),
                 Pt(puz.size, puz.size),
                 Pt(0, puz.size)]
+    print(rasterize_poly(vertices))
     include   = puz.include
     omit      = puz.omit
     tentacles = []
     for p in omit:
         mit = stalagmitable(p, include)
         tit = stalactitable(p, include, puz.size)
-        if mit and tit:
-            # Sometimes there are several Os in a row,
-            # we want to be a little smarter here not to
-            # cut the map in two!
-            if mit < tit:
-                mkStalagmite(vertices, p)
-                continue
-            else:
-                mkStalactite(vertices, p)
-                continue
         if mit:
             mkStalagmite(vertices, p)
             continue
@@ -43,11 +34,37 @@ def solve(puz: Puzzle) -> Task:
         # with the wall.
         tentacles.append(p)
     if len(tentacles) > 0:
-        assert False, ("Points", str(tentacles), " are blocked.")
+        print(":(")
+        #assert False, ("Points", str(tentacles), " are blocked.")
     return Task(border=vertices, start=Pt(0,149), obstacles=[], boosters=[])
 
 def mkStalactite(vertices, p):
-    return
+    vslen = len(vertices)
+    for k, v in enumerate(vertices):
+        if k >= vslen:
+            break
+        cx = v.x
+        cy = v.y
+        nx = vertices[k+1].x
+        ny = vertices[k+1].y
+        if cx >= p.x and nx < p.x:
+            # This implementation is analogous to mkStalagmite. Consult its comments
+            if (cy < p.y):
+                break
+            _k = k
+            if (cx == p.x):
+                vertices[_k] = Pt(v.x, p.y - 1)
+            else:
+                vertices.insert(_k + 1, Pt(p.x, cy))
+                vertices.insert(_k + 2, Pt(p.x, p.y - 1))
+                _k = _k + 2
+            if (nx == p.x - 1):
+                vertices[_k + 1] = Pt(vertices[_k + 1].x, p.y - 1)
+            else:
+                vertices.insert(_k + 1, Pt(p.x - 1, p.y - 1))
+                vertices.insert(_k + 2, Pt(p.x - 1, ny))
+            break
+
 
 def mkStalagmite(vertices, p):
     #print(['vs', vertices, 'point', p])
