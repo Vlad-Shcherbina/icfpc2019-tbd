79a5f0e1b2eb0e195c547c4fa7dafc43acebdaea
Thoughtflare <mrisaaccweiner@gmail.com>
2019-06-23 03:31:04 +0300

    Verify that golden works on big tasks, push minimum for scraper to work

--- production/golden/validate_test.py
+++ production/golden/validate_test.py
@@ -1,6 +1,17 @@
 from production import utils
-from production.golden.validate import run
+from production.golden.validate import run, do_run
 from production.golden.validate import ValidatorResult
+import os
+
+
+'''
+def test_big_solution():
+    task = (utils.project_root() / 'production' / 'golden' / '300.desc').read_text()
+    sol = (utils.project_root() / 'production' / 'golden' / 'sol-526.sol').read_text()
+    result = run(task, sol)
+    print(result)
+    assert result.time == 44556
+'''
 
 
 def test_valid_solution():
@@ -19,24 +30,24 @@ def test_invalid_solution():
     assert result.time is None
 
 
-'''
-exampleDesc = "(0,0),(10,0),(10,10),(0,10)#(0,0)#(4,2),(6,2),(6,7),(4,7);(5,8),(6,8),(6,9),(5,9)#B(0,1);B(1,1);F(0,2);F(1,2);L(0,3);X(0,9)\n".encode('utf-8')
-exampleSol1 = "WDWB(1,2)DSQDB(-3,1)DDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW\n".encode('utf-8')
-exampleWrg1 = "DWDSQDDDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW\n".encode('utf-8')
-exampleWrg2 = "DWB(1,2)DSQDB(-3,1)DDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW\n".encode('utf-8')
-exampleWrg3 = "WDWB(1,2)DSQDB(-4,1)DDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW\n".encode('utf-8')
+exampleDesc = "(0,0),(10,0),(10,10),(0,10)#(0,0)#(4,2),(6,2),(6,7),(4,7);(5,8),(6,8),(6,9),(5,9)#B(0,1);B(1,1);F(0,2);F(1,2);L(0,3);X(0,9)\n"
+exampleSol1 = "WDWB(1,2)DSQDB(-3,1)DDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW\n"
+exampleWrg1 = "DWDSQDDDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW\n"
+exampleWrg2 = "DWB(1,2)DSQDB(-3,1)DDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW\n"
+exampleWrg3 = "WDWB(1,2)DSQDB(-4,1)DDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW\n"
 
 # NB! Trailing \n is optional!
-exampleNnn1 = "WDWB(1,2)DSQDB(-3,1)DDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW".encode('utf-8')
+exampleNnn1 = "WDWB(1,2)DSQDB(-3,1)DDDWWWWWWWSSEDSSDWWESSSSSAAAAAQQWWWWWWW"
 
 exampleDescFile = os.path.join(os.path.dirname(__file__), "icfpcontest2019.github.io/download/example-01.desc")
 exampleSol1File = os.path.join(os.path.dirname(__file__), "icfpcontest2019.github.io/download/example-01-1.sol")
 
-def main():
-    assert run(exampleDesc, exampleSol1) == ValidatorResult(time=48, extra='Success')
+'''
+def test_suite():
+    assert run(exampleDesc, exampleSol1) == ValidatorResult(time=48, extra={})
 
     # NB! Trailing \n is optional!
-    assert run(exampleDesc, exampleNnn1) == ValidatorResult(time=48, extra='Success')
+    assert run(exampleDesc, exampleNnn1) == ValidatorResult(time=48, extra={})
 
     assert run(exampleDesc, exampleWrg1).time == None
     assert run(exampleDesc, exampleWrg2).time == None
