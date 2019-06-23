978693e02b6db2a682338fc3412f3beddba12519
caudate_julie <kyra-mensk@yandex.ru>
2019-06-23 09:01:05 +0300

    scratch cleanup

--- scratch/julie/__init__.py
+++ scratch/julie/__init__.py
@@ -1,6 +1,6 @@
 # See production/build_cpp_ext/README.md
 from production.build_cpp_ext import magic_extension
 magic_extension(
-    name='cpp_grid',
-    sources=['cpp_grid.cpp'],
+    name='cpp_game',
+    sources=['cpp_game.cpp'],
     headers=[])
