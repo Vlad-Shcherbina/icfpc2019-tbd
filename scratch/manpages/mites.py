d31293f306aaa34c23c70c4b327fa947842a17d8
Thoughtflare <mrisaaccweiner@gmail.com>
2019-06-23 05:43:14 +0300

    Play with tentacles a little bit

--- scratch/manpages/mites.py
+++ scratch/manpages/mites.py
@@ -20,57 +20,76 @@ def solve(puz: Puzzle) -> Task:
     tentacles = []
     for p in omit:
         mit = stalagmitable(p, include)
-        tit = stalactitable(p, include, puz.size)
         if mit is not None:
             mkStalagmite(vertices, p)
             continue
-        #if tit:
-        #    mkStalactite(vertices, p)
-        #    continue
-        # If it came to this, it means that point `p` is blocked by
-        # a must-include point both from the top and from the bottom
-        # your goal is to implemet an algorithm that will move around
-        # must-include obstacles and touch the target must-omit point
-        # with the wall.
         tentacles.append(p)
-    foosters = list(map(lambda x: Booster('B', x), tentacles))
-    foosters.extend(list(map(lambda x: Booster('R', x), include)))
-    return Task(border=vertices, start=Pt(0,149), obstacles=[], boosters=foosters)
-    if len(tentacles) > 0:
-        assert False, (
-            "Points", str(tentacles),
-            "are blocked. Closest we can get is",
-            list(map(lambda x: min(vertices, key=x.manhattan_dist), tentacles))
-        )
+    for p in tentacles:
+        if mkTentacle(vertices, include, p) is None:
+            assert False, (
+                "Point", str(p),
+                "is blocked. Closest we can get is",
+                min(vertices, key=p.manhattan_dist)
+            )
     return Task(border=vertices, start=Pt(0,149), obstacles=[], boosters=[])
 
-def mkStalactite(vertices, p):
-    vslen = len(vertices)
-    for k, v in enumerate(vertices):
-        if k >= vslen:
-            break
-        cx = v.x
-        cy = v.y
-        nx = vertices[k+1].x
-        ny = vertices[k+1].y
-        if cx >= p.x and nx < p.x:
-            # This implementation is analogous to mkStalagmite. Consult its comments
-            if (cy < p.y):
-                break
-            _k = k
-            if (cx == p.x):
-                vertices[_k] = Pt(v.x, p.y - 1)
-            else:
-                vertices.insert(_k + 1, Pt(p.x, cy))
-                vertices.insert(_k + 2, Pt(p.x, p.y - 1))
-                _k = _k + 2
-            if (nx == p.x - 1):
-                vertices[_k + 1] = Pt(vertices[_k + 1].x, p.y - 1)
-            else:
-                vertices.insert(_k + 1, Pt(p.x - 1, p.y - 1))
-                vertices.insert(_k + 2, Pt(p.x - 1, ny))
-            break
+def mkTentacle(wall, include, p):
 
+    def new_frontierX(s, p):
+        if p.y > s.y:
+            tb = 1
+        if p.y == s.y:
+            tb = 0
+        if p.y < s.y:
+            tb = -1
+        if p.x > s.x:
+            rl = 1
+        if p.x == s.x:
+            rl = 0
+        if p.x < s.x:
+            rl = -1
+        if tb != 0:
+            return (Pt(s.x, s.y + tb))
+        if rl != 0:
+            return (Pt(s.x + rl, s.y))
+        return p
+
+    def new_frontierY(s, p):
+        if p.y > s.y:
+            tb = 1
+        if p.y == s.y:
+            tb = 0
+        if p.y < s.y:
+            tb = -1
+        if p.x > s.x:
+            rl = 1
+        if p.x == s.x:
+            rl = 0
+        if p.x < s.x:
+            rl = -1
+        if tb != 0:
+            return (Pt(s.x, s.y + tb))
+        if rl != 0:
+            return (Pt(s.x + rl, s.y))
+        return p
+
+    s = min(wall, key=p.manhattan_dist)
+
+    (_s, p) = new_frontierY(s, p)
+    _ok_square = s
+    _path = []
+    while _s != p: # Work till we get there
+        while pt_in_poly(_s, wall): # Slide through the wall squares
+            _ok_square = _s         # to get to interesting stuff
+            (_s, p) = new_frontierY(_s, p)
+        if _s in include:
+            # switch direction
+        if existsExternalWallTouching(_s, wall):
+            # switch direction
+        _path.append(_s)
+        _ok_square = _s     
+        (_s, p) = new_frontierY(_s, p)
+    return None
 
 def mkStalagmite(vertices, p):
     #print(['vs', vertices, 'point', p])
@@ -118,15 +137,6 @@ def stalagmitable(p: Pt, bs: List[Pt]) -> Optional[int]:
         if b.x == p.x and b.y < p.y:
             return None
     return p.y
-
-def stalactitable(p: Pt, bs: List[Pt], h: int) -> Optional[int]:
-    for b in bs:
-        if b.x == p.x and b.y > p.y:
-            return None
-    return h - p.y
-
-def blocks(p: Pt, include: List[Pt]) -> List[Pt]:
-    return []
 
 def parseFromFile(x: str) -> Puzzle:
     y = ""
