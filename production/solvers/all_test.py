d3178c8e888a6b27575465986e7f99272ac378b3
Fj <fj.mail@gmail.com>
2019-06-24 03:53:24 +0300

    smoke-test all present solvers

---
+++ production/solvers/all_test.py
@@ -0,0 +1,22 @@
+import pytest
+from production import utils
+from production.data_formats import *
+from production.golden import validate
+
+from production.solvers.all import ALL_SOLVERS
+
+'''Smoke-test basic functionality'''
+
+
+@pytest.mark.parametrize('name, Solver', ALL_SOLVERS.items())
+def test_example1(name, Solver):
+    s = utils.get_problem_raw(1)
+    result = Solver([]).solve(s)
+    vres = validate.run(s, result.data)
+    print(vres)
+    assert vres.time == result.expected_score
+
+
+if __name__ == '__main__':
+    utils.testmod()
+
--- production/solvers/greedy_test.py
+++ production/solvers/greedy_test.py
@@ -5,17 +5,6 @@ from production.data_formats import *
 from production.solvers.greedy import solve
 from production.golden import validate
 
-def test_greedy_on_example():
-    s = Path(utils.project_root() / 'tasks' / 'part-1-examples' / 'example-01.desc').read_text()
-    task = Task.parse(s)
-    expected_score, actions, extra = solve(task)
-
-    res = validate.run(s, compose_actions(actions))
-    print(res)
-    assert res.time is not None
-    assert res.time == expected_score
-
-
 def test_greedy_with_extensions():
     s = utils.get_problem_raw(2)
     task = Task.parse(s)
